87ea29d96592716e3ef49dad140ac4693d5f8c8b
Stacey Burke <stacey.burke@telus.com>
2022-11-23 22:14:22 -0800

    add play.js

--- play.js
+++ play.js
@@ -3,20 +3,22 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+    host: 'localhost',
+    port: 50541
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log(`Server says: Connected Successfully`)
+  });
+  conn.on("data", (data) => {
+    console.log(data.toString())
+    conn.end()
+  });
   return conn;
 };
 
 console.log("Connecting ...");
 connect();
-
-conn.on("connect", () => {
-  console.log("Welcome slithery snake");
-  setName = Stacey
-}
