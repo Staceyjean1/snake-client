f98c52b89f179421b66371764a1290c019c8ad72
Stacey Burke <stacey.burke@telus.com>
2022-12-05 01:38:13 -0800

    add input.js

---
+++ input.js
@@ -0,0 +1,19 @@
+const handleUserInput = require("./handleUserInput");
+const setupInput = require("./setupInput");
+
+if (key === 'w') {
+  clearInterval(func);
+  setInterval(upkey)
+}
+if (key === 'a') {
+  clearInterval(func);
+  setInterval(leftkey)
+}
+if (key === 's') {  
+  clearInterval(func);
+  setInterval(downkey)
+}
+if (key === 'd') {
+  clearInterval(func);
+  setInterval(rightkey)
+};
